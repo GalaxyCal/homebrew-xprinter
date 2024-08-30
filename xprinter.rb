6d0b66555c488d9682a9cfaa456a02a87a327559
Apple Mac mini intel <>
2024-08-30 09:26:18 +0800

    Add xprinter formula

---
+++ xprinter.rb
@@ -0,0 +1,77 @@
+  class Xprinter < Formula
+    desc "Driver of xprinter"
+    homepage "https://github.com/GalaxyCal/homebrew-xprinter"
+    url "https://github.com/GalaxyCal/homebrew-xprinter/releases/download/v1.0.0/xprinter-1.0.0.tar.xz"  # 指向你的源代码或二进制文件
+    sha256 "cc87b1127272b1d74dc764358f1ce13c4ddfbce930d789fda7dd64776bbbed0a"  # 下载文件的SHA256校验和
+    version "1.0.0"
+  
+    def install
+      bin.install "xprinter"
+      bin.install "xprinter-server"
+    end
+  
+    def post_install
+      plist_xprinter_file = "/Library/LaunchAgents/com.max.startxprinter.plist"
+      plist_server_file = "/Library/LaunchAgents/com.max.xprinterserver.plist"
+  
+      # 创建 Launchd 配置文件
+      ohai "Creating Launchd plist file at #{plist_xprinter_file}"
+      (plist_xprinter_file).write <<~EOS
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>Label</key>
+            <string>com.max.startxprinter</string>
+            <key>ProgramArguments</key>
+            <array>
+                <string>/usr/local/bin/xprinter</string>
+                <string>server</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+        </dict>
+        </plist>
+      EOS
+  
+      ohai "Creating Launchd plist file at #{plist_server_file}"
+      (plist_server_file).write <<~EOS
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>Label</key>
+            <string>com.max.xprinterserver</string>
+            <key>ProgramArguments</key>
+            <array>
+                <string>/usr/local/bin/xprinter-server</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>KeepAlive</key>
+            <true/>
+        </dict>
+        </plist>
+      EOS
+  
+      # 设置 plist 文件权限
+      system "chmod", "755", plist_xprinter_file
+      system "chmod", "755", plist_server_file
+  
+      # 重新加载 Launchd 配置文件
+      ohai "Reloading Launchd plist file"
+      system "sudo", "launchctl", "load", plist_xprinter_file
+      system "sudo", "launchctl", "load", plist_server_file
+  
+      # 在后台运行 xprinter server 命令
+      system "nohup", "/usr/local/bin/xprinter", "server", "> /dev/null 2>&1 &"
+      # system "nohup", "/usr/local/bin/xprinter-server", "> /dev/null 2>&1 &"
+  
+      ohai "Post-install setup completed."
+    end
+  
+    test do
+      system "#{bin}/xprinter", "--version"
+    end
+  end
+  
