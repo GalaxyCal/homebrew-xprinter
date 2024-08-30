c9101e89603f2fce30381da1ef4915b6500702c1
Apple Mac mini intel <>
2024-08-30 14:07:20 +0800

    add

--- xprinter.rb
+++ xprinter.rb
@@ -2,7 +2,7 @@
     desc "Driver of xprinter"
     homepage "https://galaxycal.github.io/homebrew-xprinter/"
     url "https://github.com/GalaxyCal/homebrew-xprinter/releases/download/v1.0.0/xprinter-1.0.0.tar.xz"
-    sha256 "cc87b1127272b1d74dc764358f1ce13c4ddfbce930d789fda7dd64776bbbed0a"
+    sha256 "2bacdf67ca39448f2cf78a79f9a58c3ba0c070dd111fb90c4496c28d8572ff95"
     version "1.0.0"
   
     def install
